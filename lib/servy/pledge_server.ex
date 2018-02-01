3f382ef10f017579b447a936e252fb72c5bd828e
Peter Turner <git-commit@flintsteel7.com>
2018-01-31 18:47:58 -0800

    write test for PledgeServer

--- lib/servy/pledge_server.ex
+++ lib/servy/pledge_server.ex
@@ -59,16 +59,16 @@ defmodule Servy.PledgeServer do
 
 end
 
-alias Servy.PledgeServer
+# alias Servy.PledgeServer
 
-PledgeServer.start()
+# PledgeServer.start()
 
-IO.inspect PledgeServer.create_pledge("larry", 10)
-IO.inspect PledgeServer.create_pledge("moe", 20)
-IO.inspect PledgeServer.create_pledge("curly", 30)
-IO.inspect PledgeServer.create_pledge("daisy", 40)
-IO.inspect PledgeServer.create_pledge("grace", 50)
+# IO.inspect PledgeServer.create_pledge("larry", 10)
+# IO.inspect PledgeServer.create_pledge("moe", 20)
+# IO.inspect PledgeServer.create_pledge("curly", 30)
+# IO.inspect PledgeServer.create_pledge("daisy", 40)
+# IO.inspect PledgeServer.create_pledge("grace", 50)
 
-IO.inspect PledgeServer.recent_pledges()
+# IO.inspect PledgeServer.recent_pledges()
 
-IO.inspect PledgeServer.total_pledged()
+# IO.inspect PledgeServer.total_pledged()
---
+++ test/pledge_server_test.exs
@@ -0,0 +1,22 @@
+defmodule PledgeServerTest do
+  use ExUnit.Case, async: true
+
+  import Servy.PledgeServer
+
+  test "PledgeServer caches 3 most recent pledges" do
+    # start the PledgeServer
+    start()
+    # send some pledges
+    create_pledge("larry", 10)
+    create_pledge("moe", 20)
+    create_pledge("curly", 30)
+    create_pledge("daisy", 40)
+    create_pledge("grace", 50)
+    # prepare test list
+    most_recent_pledges = [{"grace", 50}, {"daisy", 40}, {"curly", 30}]
+    # test  the server's cache
+    assert recent_pledges() == most_recent_pledges
+    # test the server's total
+    assert total_pledged() == 120
+  end
+end
